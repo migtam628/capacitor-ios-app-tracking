3771b2d5d0696519e90c78a4d158bbac1c80c95c
Mig Tam <migtamrod@aol.com>
2020-06-26 04:10:37 -0400

    iOS 14 Update
    AppTrackingTransparency

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -6,13 +6,14 @@ import AppTrackingTransparency
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
+@available(iOS 14, *)
 @objc(CapacitorIOSAppTracking)
 public class CapacitorIOSAppTracking: CAPPlugin {
     
     @objc func requestPermission(_ call: CAPPluginCall) {
        let status = ATTrackingManager.trackingAuthorizationStatus;
-       var value;
-       ATTrackingManager.requestTrackingAuthorization { (res) in
+        var value: ATTrackingManager.AuthorizationStatus!
+        ATTrackingManager.requestTrackingAuthorization { (res) in
            value = res;
        }
         call.success([
